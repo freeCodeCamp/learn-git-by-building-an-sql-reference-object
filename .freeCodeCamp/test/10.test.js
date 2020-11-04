c32c84c4d5320a057457ba8c60e5f6985ff25a65
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 14:33:19 -0600

    10.1

---
+++ .freeCodeCamp/test/10.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  let lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+  });
+
+  it('should echo the correct text in the terminal', async () => {
+    assert(lastCommand[0] === 'echo' && lastCommand[1] === 'hello' && lastCommand[2] === 'git');
+  });
+});
