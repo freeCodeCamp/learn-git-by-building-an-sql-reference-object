8012300118d41f88a2d84936d5d802f63b48551f
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 14:32:26 -0600

    INIT

---
+++ .freeCodeCamp/test/320.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getCommits } = require('./utils');
+
+describe('You', () => {
+  it('should commit the staged files with the correct message', async () => {
+    const commits = await getCommits();
+
+    assert(commits[0].message === 'feat: add drop database reference');
+  });
+});
