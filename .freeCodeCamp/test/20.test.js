6b364d881ab9cba38ada243342ec2fa14cd73188
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 21:04:47 -0600

    30.1

--- .freeCodeCamp/test/20.test.js
+++ .freeCodeCamp/test/20.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
 
 describe('You', () => {
@@ -12,4 +12,4 @@ describe('You', () => {
   it('should use the "mkdir" command to create the correct folder', async () => {
     assert(projectDirectory.indexOf('sql_reference') >= 0 && lastCommand[0] === 'mkdir' && lastCommand[1] === 'sql_reference' && cwd === '/home/strove/project');
   });
-});
+});*/
