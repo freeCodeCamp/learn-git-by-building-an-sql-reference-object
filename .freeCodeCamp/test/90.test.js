b6445e440d776607ba0946f6abd4c692217b7014
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 21:06:10 -0600

    90.1

---
+++ .freeCodeCamp/test/90.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getDirectoryContents } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, repoDirectoryContents;
+  before(async () => {
+    repoDirectoryContents = await getDirectoryContents('../sql_reference');
+    lastCommand = await getLastCommand();
+  });
+
+  it('should use the "touch" command to create the correct file', async () => {
+    assert(lastCommand[0] === 'touch' && /README\.md/.test(lastCommand[1]) && repoDirectoryContents.indexOf('README.md' >= 0));
+  });
+});
