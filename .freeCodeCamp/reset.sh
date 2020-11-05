da201d562530961a5f9dcda56af0ac2373c71b73
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 21:05:15 -0600

    50.1

--- .freeCodeCamp/reset.sh
+++ .freeCodeCamp/reset.sh
@@ -6,3 +6,5 @@ then
 fi
 
 find ./sql_reference -not -name '.' -not -name '..' -not -name 'sql_reference' -delete
+cp -r ./.freeCodeCamp/sql_reference/. ./sql_reference
+mv ./sql_reference/dotgit ./sql_reference/.git
