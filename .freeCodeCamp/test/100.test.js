ded642abc0b715a463c20d214ac893e6b2784e73
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 21:06:29 -0600

    100.1

---
+++ .freeCodeCamp/test/100.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "README.md" file', () => {
+  let readmeFile;
+  before(async () => {
+    readmeFile = await getFileContents('../sql_reference/README.md');
+  });
+
+  it('should have the correct text added', async () => {
+    assert(/SQL Reference/.test(readmeFile));
+  });
+});
