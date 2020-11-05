122b0f17c2061326a366d010321f30a9325e131d
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 21:06:29 -0600

    100.1

--- .freeCodeCamp/test/90.test.js
+++ .freeCodeCamp/test/90.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
   it('should use the "touch" command to create the correct file', async () => {
     assert(lastCommand[0] === 'touch' && /README\.md/.test(lastCommand[1]) && repoDirectoryContents.indexOf('README.md' >= 0));
   });
-});
+});*/
