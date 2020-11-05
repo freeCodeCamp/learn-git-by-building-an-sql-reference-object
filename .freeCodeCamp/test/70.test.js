fbc4c2d8cce62bf998450d4dd5331e3c2e4391bc
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-04 21:05:44 -0600

    70.1

---
+++ .freeCodeCamp/test/70.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getCommandOutput } = require('./utils');
+
+describe('You', () => {
+  let commandOutput;
+  before(async () => {
+
+    commandOutput = await getCommandOutput('git status');
+  });
+
+  it('should correctly switch to a branch named "main"', async () => {
+    assert(/On branch main\s/.test(commandOutput));
+  });
+});
