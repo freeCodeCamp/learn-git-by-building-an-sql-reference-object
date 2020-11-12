c14818ccebb3790258a476bd09ade59b68c02302
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 14:20:34 -0600

    280.1

--- .freeCodeCamp/test/270.test.js
+++ .freeCodeCamp/test/270.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
   it('should check the log of your repository', async () => {
     assert(lastCommand[0] === 'git' && lastCommand[1] === 'log' && cwd === '/home/strove/project/sql_reference');
   });
-});
+});*/
