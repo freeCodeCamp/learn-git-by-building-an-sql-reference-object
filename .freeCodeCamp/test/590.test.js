1186ff6ccf3bb4ee5957db676f1f30173ce1b20b
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 15:02:16 -0600

    590.1

---
+++ .freeCodeCamp/test/590.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommandOutput } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commandOutput;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    commandOutput = await getCommandOutput('git branch');
+  });
+
+  it('should use "git checkout" to switch to the correct branch', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && (lastCommand[2] === '-D' || lastCommand[2] === '-d') && !/feat\/add-drop-table-reference\s/.test(commandOutput));
+  });
+});
