7f38d60095018e54190d5f04f527ef1b2b73b42e
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-11 19:00:26 -0600

    120.1

---
+++ .freeCodeCamp/test/120.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { getStagedFiles } = require('./utils');
+
+describe('You', () => {
+  let stagedFiles;
+  before(async () => {
+    stagedFiles = await getStagedFiles();
+  });
+
+  it('should add your "README.md" file to staging', async () => {
+    const stagedFileIndex = stagedFiles.findIndex(item => {
+      return item.filename === 'README.md' && item.status === 'Added';
+    });
+
+    assert(stagedFileIndex >= 0);
+  });
+});
