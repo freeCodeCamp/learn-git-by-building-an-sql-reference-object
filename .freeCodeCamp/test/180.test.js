ce9f5a3969bb77297fcb4c42cae04f1aa0fbe158
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 11:47:16 -0600

    180.1

---
+++ .freeCodeCamp/test/180.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommits } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commits;
+  before(async () => {
+    commits = await getCommits();
+    lastCommand = await getLastCommand();
+  });
+
+  it('should commit the staged files', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'commit' && commits.length > 0);
+  });
+});
