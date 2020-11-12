8a2261fa7c09fe778125d30e1919cca199008a38
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 14:46:50 -0600

    460.1

---
+++ .freeCodeCamp/test/460.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should view the branches in your repository', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && cwd === '/home/strove/project/sql_reference');
+  });
+});
