b3a882ce0c557755711e54f591efce8745c1659e
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 14:32:02 -0600

    350.1

---
+++ .freeCodeCamp/test/350.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommandOutput } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commandOutput;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    commandOutput = await getCommandOutput('git branch');
+  });
+
+  it('should use the "git branch" command to create the correct branch', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && /feat\/add-create-table-reference/.test(commandOutput));
+  });
+});
