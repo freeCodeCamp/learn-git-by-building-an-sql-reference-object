6ca41aaa3ef38790c7b9bc50bbd2c464868c5fd5
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 14:51:54 -0600

    500.1

--- .freeCodeCamp/test/490.test.js
+++ .freeCodeCamp/test/490.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCommandOutput } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
   it('should use "git checkout" to switch to the correct branch', async () => {
     assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && (lastCommand[2] === '-D' || lastCommand[2] === '-d') && !/feat\/add-create-table-reference/.test(commandOutput));
   });
-});
+});*/
