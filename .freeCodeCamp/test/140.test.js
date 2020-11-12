3407948898c4ce47e4c599f549f9f05001da212a
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 11:30:42 -0600

    150.1

--- .freeCodeCamp/test/140.test.js
+++ .freeCodeCamp/test/140.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents } = require('./utils');
 
 describe('You', () => {
@@ -8,4 +8,4 @@ describe('You', () => {
 
     assert(lastCommand[0] === 'touch' && /sql_reference\.json$/.test(lastCommand[1]) && repoDirectoryContents.indexOf('sql_reference.json' >= 0));
   });
-});
+});*/
