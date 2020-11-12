b38f2c4377d8397ae3fc4415798fd43c155b26bf
moT01 <20648924+moT01@users.noreply.github.com>
2020-11-12 14:20:34 -0600

    280.1

---
+++ .freeCodeCamp/test/280.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "sql_reference.json" file', () => {
+  let jsonFile, parsedJson;
+  before(async () => {
+    jsonFile = await getFileContents('../sql_reference/sql_reference.json');
+    parsedJson = await JSON.parse(jsonFile);
+  });
+
+  it('should have the correct property and value added', async () => {
+    assert(parsedJson.database.drop === 'DROP DATABASE database_name;');
+  });
+});
