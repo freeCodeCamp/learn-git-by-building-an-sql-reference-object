711ea0720f553d5e5c6aef1b55b182cc5282e0aa
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-21 21:48:55 -0600

    740.1

---
+++ .freeCodeCamp/test/740.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should merge the correct branch into the main branch', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'merge' && lastCommand[2] === 'fix/create-table-syntax');
+  });
+});
