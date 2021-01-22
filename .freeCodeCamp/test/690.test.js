5f4e5a1e058178bb953bce47906fe755933418d4
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-21 21:45:18 -0600

    700.1

--- .freeCodeCamp/test/690.test.js
+++ .freeCodeCamp/test/690.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your "sql_reference.json" file', () => {
@@ -11,4 +11,4 @@ describe('Your "sql_reference.json" file', () => {
   it('should have the correct properties and values added', async () => {
     assert(parsedJson.table.create === 'CREATE TABLE table_name();');
   });
-});
+});*/
