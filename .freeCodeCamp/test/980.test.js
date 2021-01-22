ca2223ef8861bc89f03b60591990a630a0d11275
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-21 22:07:02 -0600

    980.1

---
+++ .freeCodeCamp/test/980.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "sql_reference.json" file', () => {
+  let jsonFile, copyOfJsonFile;
+  before(async () => {
+    jsonFile = await getFileContents('../sql_reference/sql_reference.json');
+    copyOfJsonFile = await getFileContents('./test/sample_files/sql_reference.json');
+  });
+
+  it('should have the correct properties and values', async () => {
+    assert(jsonFile === copyOfJsonFile);
+  });
+});
