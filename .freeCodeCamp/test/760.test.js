8545fd9ebb4b84f0c2ad9a1b22c5808fd1ed0408
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-21 21:50:32 -0600

    760.1

---
+++ .freeCodeCamp/test/760.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommandOutput } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commandOutput;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    commandOutput = await getCommandOutput('git status');
+  });
+
+  it('should use "git checkout" to switch to the correct branch', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'checkout' && /On branch feat\/add-column-references/.test(commandOutput));
+  });
+});
