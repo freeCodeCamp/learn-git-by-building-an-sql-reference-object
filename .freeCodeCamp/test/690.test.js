603b60534ada72e26160840df63b34759334360e
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-21 21:44:55 -0600

    690.1

---
+++ .freeCodeCamp/test/690.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "sql_reference.json" file', () => {
+  let jsonFile, parsedJson;
+  before(async () => {
+    jsonFile = await getFileContents('../sql_reference/sql_reference.json');
+    parsedJson = await JSON.parse(jsonFile);
+  });
+
+  it('should have the correct properties and values added', async () => {
+    assert(parsedJson.table.create === 'CREATE TABLE table_name();');
+  });
+});
