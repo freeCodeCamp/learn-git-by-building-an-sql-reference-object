9b75e907f7ff1f3a02db565cab4250bbed73d02d
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-21 21:46:57 -0600

    710.1

---
+++ .freeCodeCamp/test/710.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommits } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commits;
+  before(async () => {
+    commits = await getCommits();
+    lastCommand = await getLastCommand();
+  });
+
+  it('should commit the staged files with the correct message', async () => {
+    assert(commits[0].message === 'fix: create table syntax');
+  });
+});
