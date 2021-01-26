ecf95142a1fd9e8017be58561ecd31e8f99645d6
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 14:09:34 -0600

    755.1

---
+++ .freeCodeCamp/test/755.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommandOutput } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commandOutput;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    commandOutput = await getCommandOutput('git branch');
+  });
+
+  it('should delete the correct branch', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && (lastCommand[2] === '-D' || lastCommand[2] === '-d') && !/fix\/create-table-syntax\s/.test(commandOutput));
+  });
+});
