d167f4b827d9e523ad0227d54700d3a2572c65e9
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:29:29 -0600

    1880.1

---
+++ .freeCodeCamp/test/1880.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getDirectoryContents } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, repoDirectoryContents;
+  before(async () => {
+    repoDirectoryContents = await getDirectoryContents('../sql_reference');
+    lastCommand = await getLastCommand();
+  });
+
+  it('should use the "touch" command to create the correct file', async () => {
+    assert(lastCommand[0] === 'touch' && /sample\.env$/.test(lastCommand[1]) && repoDirectoryContents.indexOf('sample.env' >= 0));
+  });
+});
