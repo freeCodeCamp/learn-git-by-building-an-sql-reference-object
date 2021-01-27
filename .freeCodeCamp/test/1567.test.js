bafc3a671baf3e9e7b6efdf536dad927bfc9688c
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:08:29 -0600

    1570.1

--- .freeCodeCamp/test/1567.test.js
+++ .freeCodeCamp/test/1567.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getNextCommand } = require('./utils');
 
 describe('You', () => {
@@ -9,4 +9,4 @@ describe('You', () => {
 
     assert(nextCommand[0] === 'git' && nextCommand[1] === 'rebase' && (args1 || args2));
   });
-});
+});*/
