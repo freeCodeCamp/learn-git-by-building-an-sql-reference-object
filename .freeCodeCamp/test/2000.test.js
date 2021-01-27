c2a51a2a73ade55c5ddb1d83eb69a8bf9bce73d9
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:37:06 -0600

    2000.1

---
+++ .freeCodeCamp/test/2000.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should check the log of your repository', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'log' && lastCommand[2] === undefined && cwd === '/home/strove/project/sql_reference');
+  });
+});
