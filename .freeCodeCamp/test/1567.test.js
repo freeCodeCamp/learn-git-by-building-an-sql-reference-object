0f19049a997db155d489d68cec8952f1d489b822
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:08:18 -0600

    1567.1

---
+++ .freeCodeCamp/test/1567.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getNextCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter an interactive rebase', async () => {
+    const nextCommand = await getNextCommand();
+    const args1 = (nextCommand[2] === '-i' || nextCommand[2] === '--interactive') && nextCommand[3] === '--root';
+    const args2 = nextCommand[2] === '--root' && (nextCommand[3] === '-i' || nextCommand[3] === '--interactive');
+
+    assert(nextCommand[0] === 'git' && nextCommand[1] === 'rebase' && (args1 || args2));
+  });
+});
