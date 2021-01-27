2feb9d2567fc1a47e67955e0ad688949b2b72988
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:08:29 -0600

    1570.1

---
+++ .freeCodeCamp/test/1570.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('You', () => {
+  let rebaseFile;
+  before(async () => {
+    rebaseFile = await getFileContents('../sql_reference/.git/rebase-merge/done');
+  });
+
+  it('should use the "reword" option', async () => {
+    assert(/^(r|reword) /gm.test(rebaseFile));
+  });
+});
