1acfcadeea9ecd7f984ecd263409b31cebbba5e3
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 21:42:46 -0600

    1150.1

--- .freeCodeCamp/test/1150.test.js
+++ .freeCodeCamp/test/1150.test.js
@@ -1,14 +1,14 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCommandOutput } = require('./utils');
 
 describe('You', () => {
   let lastCommand, commandOutput;
   before(async () => {
     lastCommand = await getLastCommand();
-    commandOutput = await getCommandOutput('git status');
+    commandOutput = await getCommandOutput('git branch');
   });
 
   it('should use "git checkout" to switch to the correct branch', async () => {
-    assert(lastCommand[0] === 'git' && lastCommand[1] === 'checkout' && /On branch main\s/.test(commandOutput));
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && (lastCommand[2] === '-D' || lastCommand[2] === '-d') && !/feat\/add-insert-row-reference\s/.test(commandOutput));
   });
-});*/
+});
