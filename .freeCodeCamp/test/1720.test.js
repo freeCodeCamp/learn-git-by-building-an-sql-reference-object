34cb5b99f39d3442958d9e25e599988083c36e2f
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:15:28 -0600

    1720.1

---
+++ .freeCodeCamp/test/1720.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should enter an interactive rebase', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'rebase' && (lastCommand[2] === '-i' || lastCommand[2] === '--interactive') && lastCommand[3] === '--root' && cwd === '/home/strove/project/sql_reference');
+  });
+});
