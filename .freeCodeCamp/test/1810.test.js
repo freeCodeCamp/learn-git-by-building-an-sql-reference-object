cb13ad0ea789a30cfb298a4d818ff62e43332c57
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:24:21 -0600

    1810.1

---
+++ .freeCodeCamp/test/1810.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your ".env" file', () => {
+  let envFile;
+  before(async () => {
+    envFile = await getFileContents('../sql_reference/.env');
+  });
+
+  it('should have the correct text added', async () => {
+    assert(/^SECRET=MY_SECRET\s*?$/.test(envFile));
+  });
+});
