6c2260c6048a30368db8ebfb25b1e83435f282a4
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 21:44:33 -0600

    1170.1

--- .freeCodeCamp/test/1160.test.js
+++ .freeCodeCamp/test/1160.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getCommandOutput } = require('./utils');
 
 describe('You', () => {
@@ -8,4 +8,4 @@ describe('You', () => {
 
     assert(lastCommand[0] === 'git' && lastCommand[1] === 'checkout' && /On branch feat\/add-more-row-references\s/.test(commandOutput));
   });
-});
+});*/
