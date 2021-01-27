591d79369853865589f06e3e60cba670d3ef4b5a
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:32:09 -0600

    1930.1

---
+++ .freeCodeCamp/test/1930.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { getStagedFiles } = require('./utils');
+
+describe('You', () => {
+  let stagedFiles;
+  before(async () => {
+    stagedFiles = await getStagedFiles();
+  });
+
+  it('should add the correct file to staging', async () => {
+    const stagedFileIndex = stagedFiles.findIndex(item => {
+      return item.filename === 'sample.env' && item.status === 'Added';
+    });
+
+    assert(stagedFileIndex >= 0);
+  });
+});
