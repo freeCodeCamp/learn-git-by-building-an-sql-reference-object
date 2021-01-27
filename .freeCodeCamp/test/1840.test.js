295d8cc6471ca3ddbc57cef3385e291cd69d3841
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:27:07 -0600

    1840.1

---
+++ .freeCodeCamp/test/1840.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your ".gitignore" file', () => {
+  let gitignoreFile;
+  before(async () => {
+    gitignoreFile = await getFileContents('../sql_reference/.gitignore');
+  });
+
+  it('should have the correct text added', async () => {
+    assert(/^\.env\s*?$/.test(gitignoreFile));
+  });
+});
