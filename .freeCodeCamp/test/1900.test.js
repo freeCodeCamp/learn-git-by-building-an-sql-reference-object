f1b175780d2b3179a572308e38c69d5ef567c5a8
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 22:30:45 -0600

    1900.1

---
+++ .freeCodeCamp/test/1900.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your ".gitignore" file', () => {
+  let sampleEnvFile;
+  before(async () => {
+    sampleEnvFile = await getFileContents('../sql_reference/sample.env');
+  });
+
+  it('should have the correct text added', async () => {
+    assert(/^SECRET=\s*?$/.test(sampleEnvFile));
+  });
+});
