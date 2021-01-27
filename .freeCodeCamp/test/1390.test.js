2da689ff9701ae62d5f298f30010ac49cfc7d87d
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 21:58:18 -0600

    1390.1

---
+++ .freeCodeCamp/test/1390.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getCommits } = require('./utils');
+
+describe('You', () => {
+  let commits;
+  before(async () => {
+    commits = await getCommits();
+  });
+
+  it('should commit the staged files with the correct message', async () => {
+    assert(commits[0].message === 'feat: add delete row reference');
+  });
+});
