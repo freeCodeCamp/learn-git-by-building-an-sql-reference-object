bf508de8f3c7e947ce99cc8f21d0f330aa2f9952
moT01 <20648924+moT01@users.noreply.github.com>
2021-01-26 21:36:10 -0600

    1015.1

---
+++ .freeCodeCamp/test/1015.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your "sql_reference.json" file', () => {
+  let jsonFile, parsedJson;
+  before(async () => {
+    jsonFile = await getFileContents('../sql_reference/sql_reference.json');
+    parsedJson = await JSON.parse(jsonFile);
+  });
+
+  it('should have the correct properties and values added', async () => {
+    assert(parsedJson.row.update === 'UPDATE table_name SET column_name = new_value WHERE condition;');
+  });
+});
