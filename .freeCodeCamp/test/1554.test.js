7bf5453f628b4563bfb657b2e26d32d908b3aa79
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-05 12:15:54 -0600

    1554.1

---
+++ .freeCodeCamp/test/1554.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should "show" the changes of the correct commit', async () => {
+    const lastCommand2 = lastCommand[2] === undefined || lastCommand[2] === 'HEAD';
+
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'show' && lastCommand2 && cwd === '/home/strove/project/sql_reference');
+  });
+});
