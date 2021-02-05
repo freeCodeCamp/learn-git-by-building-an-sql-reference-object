b520c486ee7cf20f28bb2a5d004440e3d289771f
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-05 12:14:19 -0600

    1548.1

---
+++ .freeCodeCamp/test/1548.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getNextCommand } = require('./utils');
+
+describe('You', () => {
+  let nextCommand;
+  before(async () => {
+    nextCommand = await getNextCommand();
+  });
+
+  it('should use the "revert" command correctly', async () => {
+    assert(false && nextCommand[0] === 'git' && nextCommand[1] === 'revert' && cwd === '/home/strove/project/sql_reference');
+  });
+});
