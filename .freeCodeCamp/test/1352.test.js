d5ac2191198a065489121e628a8a0727e16881e5
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-05 12:00:46 -0600

    1354.1

--- .freeCodeCamp/test/1352.test.js
+++ .freeCodeCamp/test/1352.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your "sql_reference.json" file', () => {
@@ -11,4 +11,4 @@ describe('Your "sql_reference.json" file', () => {
   it('should have the correct properties and values added', async () => {
     assert(parsedJson.column.foreign_key === 'ALTER TABLE table_name ADD FOREIGN KEY(column_name) REFERENCES table_name(column_name);');
   });
-});
+});*/
