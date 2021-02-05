3b7bd46cdd32ccafcee584a6ce01b74b1370973f
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-05 12:14:52 -0600

    1551.1

--- .freeCodeCamp/test/1549.test.js
+++ .freeCodeCamp/test/1549.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getCommits } = require('./utils');
 
 describe('You', () => {
@@ -10,4 +10,4 @@ describe('You', () => {
   it('should have the correct last commit message', async () => {
     assert(/^Revert "feat: add unique reference"\s/.test(commits[0].message));
   });
-});
+});*/
