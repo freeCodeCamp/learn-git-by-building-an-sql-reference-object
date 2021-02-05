4ac486761abe7ef5873c9c3e3f90595b1302f7ae
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-05 12:19:32 -0600

    1563.1

---
+++ .freeCodeCamp/test/1563.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getCommits } = require('./utils');
+
+describe('You', () => {
+  let commits;
+  before(async () => {
+    commits = await getCommits();
+  });
+
+  it('should have the correct last commit message', async () => {
+    assert(commits[0].message === 'feat: add foreign key reference');
+  });
+});
