45d153614c7b8a54e271a5d2ac0e2a8f5e5f433d
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-06 11:58:38 -0600

    1113.1

--- .freeCodeCamp/test/1113.test.js
+++ .freeCodeCamp/test/1113.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -6,6 +6,6 @@ describe('You', () => {
     const lastCommand = await getLastCommand();
     const cwd = await getCwd();
 
-    assert(lastCommand[0] === 'git' && lastCommand[1] === 'stash' && lastCommand[2] === 'list' && /\/project\/sql_reference$/.test(cwd));
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'stash' && lastCommand[2] === 'show' && lastCommand[3] === 'stash@{1}' && /\/project\/sql_reference$/.test(cwd));
   });
-});*/
+});
