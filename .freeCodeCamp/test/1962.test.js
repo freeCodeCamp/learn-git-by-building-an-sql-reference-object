debfc909145e93a94293e204dc05086651d94a4f
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-09 17:42:29 -0600

    1962.1

---
+++ .freeCodeCamp/test/1962.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommits } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commits;
+  before(async () => {
+    commits = await getCommits();
+    lastCommand = await getLastCommand();
+  });
+
+  it('should have the correct last commit message', async () => {
+    assert(/^feat: add .gitignore and sample.env\s/.test(commits[0].message));
+  });
+});
