e95c53a4c123e7d61433c539f96755aa7cffc8b2
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-09 17:27:04 -0600

    1572.1

---
+++ .freeCodeCamp/test/1572.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getCommits } = require('./utils');
+
+describe('You', () => {
+  let commits;
+  before(async () => {
+    commits = await getCommits();
+  });
+
+  it('should choose the commits to interact with', async () => {
+    assert(commits[4].message === 'feat: add column references');
+  });
+});
