f6c21fc9f38e13acfa31fea96f1b1b23a3e6933b
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-09 17:27:04 -0600

    1572.1

--- .freeCodeCamp/test/1570.test.js
+++ .freeCodeCamp/test/1570.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('You', () => {
@@ -7,4 +7,4 @@ describe('You', () => {
 
     assert(/^(r|reword) /gm.test(rebaseFile));
   });
-});
+});*/
