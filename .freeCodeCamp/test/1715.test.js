44363b17c4ed10b7ed8ede6901784091c4f006a4
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-09 17:07:08 -0600

    1715.1

---
+++ .freeCodeCamp/test/1715.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should check the log of your repository', async () => {
+    const lastCommand = await getLastCommand();
+    const cwd = await getCwd();
+    const args1 = lastCommand[2] === '-5' && lastCommand[3] === '--oneline';
+    const args2 = lastCommand[2] === '--oneline' && lastCommand[3] === '-5';
+
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'log' && (args1 || args2) && /\/project\/sql_reference$/.test(cwd));
+  });
+});
