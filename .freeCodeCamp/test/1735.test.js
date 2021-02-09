ce3824ed52afca85665b52d39cd40d34770e7deb
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-09 17:34:32 -0600

    1735.1

---
+++ .freeCodeCamp/test/1735.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCommits } = require('./utils');
+
+describe('You', () => {
+  let lastCommand, commits;
+  before(async () => {
+    commits = await getCommits();
+    lastCommand = await getLastCommand();
+  });
+
+  it('should have the correct last commit message', async () => {
+    assert(/^fix: add missing rename references\s/.test(commits[0].message));
+  });
+});
